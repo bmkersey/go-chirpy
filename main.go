005f2f0b4d8683b568b9bda52a19b87680e6cbfb
Brendan <bmkersey@gmail.com>
2025-04-13 14:50:21 -0400

    added a few routes, healthz metrics and reset

--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 
 func main(){
-
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	serverMux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	
+	serverMux.HandleFunc("GET /api/healthz", healthHandler)
+
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: serverMux,
 	}
 
+
+
+	
+
 	server.ListenAndServe()
 
 
+
 }
+
+
