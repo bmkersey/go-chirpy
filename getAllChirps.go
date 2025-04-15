5c1dd340a8b0676e7b9177ec1cfe4b39c538127e
Brendan <bmkersey@gmail.com>
2025-04-15 13:16:25 -0400

    added optional query params to fetch chirps by author and to change the sorting

--- getAllChirps.go
+++ getAllChirps.go
@@ -3,25 +3,51 @@ package main
 import (
 	"log"
 	"net/http"
+	"sort"
+
+	"github.com/bmkersey/go-chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
-
 func (c *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	chirps, err := c.dbQueries.GetAllChirps(r.Context())
-	if err != nil {
-		log.Printf("Error retrieving chirps: %s", err)
-		sendError(w, 400, "Something went wrong")
+	authorID := r.URL.Query().Get("author_id")
+	sortOrder := r.URL.Query().Get("sort")
+	var chirps []database.Chirp
+	var err error
+
+	if authorID == "" {
+		chirps, err = c.dbQueries.GetAllChirps(r.Context())
+		if err != nil {
+			log.Printf("Error retrieving chirps: %s", err)
+			sendError(w, 400, "Something went wrong")
+		}
+	} else {
+		authorUUID, err := uuid.Parse(authorID)
+		if err != nil {
+			sendError(w, 400, "Could not turn provided author ID to UUID")
+			return
+		}
+		chirps, err = c.dbQueries.GetChirpsByAuthor(r.Context(), authorUUID)
+		if err != nil {
+			log.Printf("Error retrieving chirps: %s", err)
+			sendError(w, 400, "Something went wrong")
+		}
 	}
 
 	chirpsRes := []Chirp{}
-	for _, chirp := range chirps{
-		chirpsRes = append(chirpsRes,Chirp{
-			ID: chirp.ID,
+	for _, chirp := range chirps {
+		chirpsRes = append(chirpsRes, Chirp{
+			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,
 			UpdatedAt: chirp.UpdatedAt,
-			Body: chirp.Body,
-			User_id: chirp.UserID,
+			Body:      chirp.Body,
+			User_id:   chirp.UserID,
+		})
+	}
 
+	if sortOrder == "desc" {
+		sort.Slice(chirpsRes, func(i, j int) bool {
+			return chirpsRes[j].CreatedAt.Before(chirpsRes[i].CreatedAt)
 		})
 	}
 
